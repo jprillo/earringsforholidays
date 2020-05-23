a97528456d45770d41eb578ce6105b90051d8fec
Jason Prillo <jasonprillo@gmail.com>
2020-05-23 18:17:15 -0400

    add responsive NavBar pack

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,51 @@
+
+  import React from "react"
+  import {Link} from "gatsby"
+  
+  
+  const activeStyle = {
+      color: '#FF6AB2',
+    }
+  
+  
+  const NavBar = ({ toggleNavbar, isActive }) => {    
+      return(
+          <div className={` navagation-wrap col-12 ${isActive ? 'mobile-wrap' : ''}`}> 
+  
+              <div id="main-nav-wrap">
+                  <div className="logo-wrap">
+                      <Link to="/">
+                    <span style={{color: "black", fontWeight: "bold"}}>Wine</span><span className="primary-color">Catalog</span> </Link>
+  
+                  </div>
+                  <div >
+                      <div  className={` responsive-nav ${isActive ? 'mobile-nav' : ''}`}>
+                          <ul className="nav">
+                              <li><Link className="hack" activeStyle={activeStyle} to="/">Home</Link></li>
+                              <li><Link to="/about/" activeStyle={activeStyle} className="hack">About</Link></li>
+                              <li><Link to="/blog/" activeStyle={activeStyle} className="hack">Catalog</Link></li>
+                              <li><Link to="/contact/" activeStyle={activeStyle} className="hack">Contact</Link></li>
+                           
+  
+                            
+                          </ul>
+                      </div>
+                   
+                  </div>
+                  <div id="hamburger-wrap">
+                      <div  className={` burger-open ${isActive ? 'burger-close' : ''}`}
+              
+              data-target='nav-menu'
+              onClick={toggleNavbar}
+              aria-hidden={true}>
+                          <span className="line line01"></span>
+                          <span className="line line02"></span>
+                          <span className="line line03"></span>
+                      </div>
+                  </div>
+              </div>
+          </div>
+  
+      )
+  }
+  export default NavBar
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import "../style/main.scss"
+import NavBar from "../components/NavBar/NavBar";
 
 export default class MainLayout extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { isActive: false }
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+  }
+
+  toggleNavbar () {
+    this.setState({ isActive: !this.state.isActive })
+  }
+ 
   render() {
     const { children } = this.props;
     return (
@@ -12,7 +23,7 @@ export default class MainLayout extends React.Component {
           <meta name="description" content={config.siteDescription} />
           <html lang="en" />
         </Helmet>
-       
+        <NavBar isActive={this.state.isActive} toggleNavbar={() => this.toggleNavbar()} />
         {children}
       </div>
     );
